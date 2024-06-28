333ef6415d298ab3baac2c94028b684a3592fb53
carsandclo <cloandcars@gmail.com>
2024-06-28 23:41:44 +0700

    Update PlasmicHomepage.tsx

--- components/plasmic/blank_website_mobile_first/PlasmicHomepage.tsx
+++ components/plasmic/blank_website_mobile_first/PlasmicHomepage.tsx
@@ -17,6 +17,9 @@ import Head from "next/head";
 import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/router";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 import {
